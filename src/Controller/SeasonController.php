5bae7dded27cec2c9ed832fcb281847a99213b10
Guilherme Rafael Andrade Lima <guilima102013@gmail.com>
2024-03-07 16:29:50 -0300

    Utilizando cache p/ otimizar queries p/ buscar os dados de QTD de episódios

--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -6,13 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Series;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class SeasonController extends AbstractController
 {
+    public function __construct(private CacheInterface $cache){
+        
+    }
+
     #[Route('/series/{series}/seasons', name: 'app_season')]
     public function index(Series $series): Response
     {
-        $seasons = $series->getSeasons();
+        $seasons = $this->cache->get(
+            "series_{$series->getId()}_seasons",
+            function (ItemInterface $item) use ($series) {
+                $item->expiresAfter(new \DateInterval('PT10S'));
+
+                /** @var PersistentCollection $seasons */
+                $seasons = $series->getSeasons();
+                $seasons->initialize();
+
+                return $seasons;
+            }
+        );
+
+        // dd($seasons->count());
+
+        // $seasons = $series->getSeasons();
 
         return $this->render('season/index.html.twig', [
             'seasons' => $seasons,
